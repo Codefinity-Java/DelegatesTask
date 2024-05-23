28a7ba9e952c2cff2b40e5663227fcd41f9c3931
Muhammad Mouiz Ghouri <mouizghouri77@gmail.com>
2024-05-23 13:45:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace DelegatesTask
+{
+    internal class Program
+    {
+        // Write code below this line
+
+        // Write code above this line
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine(arrayMethod(max, nums));
+            Console.WriteLine(arrayMethod(avg, nums));
+        }
+
+        static float arrayMethod(Operation opr, int[] nums)
+        {
+            return opr(nums);
+        }
+
+        static int max(int[] nums)
+        {
+            int max = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+                if (max < nums[i])
+                    max = nums[i];
+            return max;
+        }
+
+        static float avg(int[] nums)
+        {
+            float sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+                sum += nums[i];
+            return sum / nums.Length;
+        }
+    }
+}
